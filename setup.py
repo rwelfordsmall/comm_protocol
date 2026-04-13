987ea3814a3b849830a8e39322afbb8e0a653e8d
rwelf <rwelfordsmall@gmail.com>
2026-04-13 10:54:03 -0400

    wifi/eth/test

--- setup.py
+++ setup.py
@@ -24,12 +24,16 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'lora_sender   = comm_protocol.lora_sender_node:main',
-            'lora_receiver   = comm_protocol.lora_receiver_node:main',
-            'heartbeat_node = comm_protocol.heartbeat_node:main',
-            'joy_bridge_node = comm_protocol.joy_bridge_node:main',
-            'goal_bridge_node = comm_protocol.goal_bridge_node:main',
-            'lora_bridge = comm_protocol.lora_bridge_node:main',
+            'lora_sender        = comm_protocol.lora_sender_node:main',
+            'lora_receiver      = comm_protocol.lora_receiver_node:main',
+            'lora_bridge        = comm_protocol.lora_bridge_node:main',
+            'heartbeat_node     = comm_protocol.heartbeat_node:main',
+            'joy_bridge_node    = comm_protocol.joy_bridge_node:main',
+            'goal_bridge_node   = comm_protocol.goal_bridge_node:main',
+            'wifi_sender_node   = comm_protocol.wifi_sender_node:main',
+            'wifi_receiver_node = comm_protocol.wifi_receiver_node:main',
+            'eth_sender_node    = comm_protocol.eth_sender_node:main',
+            'eth_receiver_node  = comm_protocol.eth_receiver_node:main',
         ],
     },
 )
